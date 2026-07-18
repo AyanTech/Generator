6bed4927c36c4ec6365424e18b91d9906846416b
Farshid <Farshid.roohi.a@Gmail.com>
2026-07-18 13:24:43 +0330

    Sample: Make a sample for use generator and v2 networking

--- app/src/main/java/com/alirezabdn/generator/OnlyInputApi.kt
+++
@@ -1,9 +0,0 @@
-package com.alirezabdn.generator
-
-import kotlinx.serialization.Serializable
-
-@AyanAPI(endPoint = "DesiredName")
-class OnlyInputApi {
-    @Serializable
-    data class OnlyRequestBody(val test: String)
-}
